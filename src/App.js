e3b9ea451d1421b82db82b2257ba055ccc95b85b
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-03-20 15:08:48 -0700

    Finished creating variable and 3 objects inside array and JS expression inside JSX.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 function App() {
 	
+	const todoList = [];
+	todoList.push( {id: 1, title: "Complete assignment"} );
+	todoList.push( {id: 2, title: "Watch video lectures"} );
+	todoList.push( {id: 3, title: "Go to mentor sessions"} );
+	todoList.push( {id: 4, title: "Do mindset exercises"} );
+	todoList.push( {id: 5, title: "Go to sleep"} );
+
+
   return (
+
 		<div>
 			<h1>Todo List</h1>
 
 			<ul>
-				<li>Test list item 1</li>
 			</ul>
+
+			{/* and by the way: that's how you do comments in JSX */}
+
 		</div>
+
   );
 }
 
