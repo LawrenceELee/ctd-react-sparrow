6fe0a33cea0332b0504956ad77cba205e0f8e89e
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-04-01 00:17:32 -0700

    Moved JSX, todoList array, to TodoList.js and also refactored App.js to use TodoList component. Verified that code is still working.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from 'react';
+import TodoList from './TodoList';
 
 function App() {
 	
-	const todoList = [];
-	todoList.push( {id: 1, title: "Complete assignment"} );
-	todoList.push( {id: 2, title: "Watch video lectures"} );
-	todoList.push( {id: 3, title: "Go to mentor sessions"} );
-	todoList.push( {id: 4, title: "Do mindset exercises"} );
-	todoList.push( {id: 5, title: "Go to sleep"} );
-
 
   return (
 
 		<div>
 			<h1>Todo List</h1>
 
-			<ul>
-				{/* todoList.map( function (e) {
-					return <li key={e.id}>{e.title}</li>;
-				}) */}
-				
-				{/* refactor using fat arrow syntax */}
-				{todoList.map(
-					(e) => {
-						return <li key={e.id}>{e.title}</li>;
-					})
-				}
-			</ul>
-
-			<ul>
-				<li> item 1 test </li>
-			</ul>
-
-			{/* and by the way: that's how you do comments in JSX */}
-
+			<TodoList />
 		</div>
 
   );
 }
+
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 
 function TodoList() {
+
+	const todoList = [];
+	todoList.push( {id: 1, title: "Complete assignment"} );
+	todoList.push( {id: 2, title: "Watch video lectures"} );
+	todoList.push( {id: 3, title: "Go to mentor sessions"} );
+	todoList.push( {id: 4, title: "Do mindset exercises"} );
+	todoList.push( {id: 5, title: "Go to sleep"} );
+
 	return (
 		<div>
+			<ul>
+				{/* todoList.map( function (e) {
+					return <li key={e.id}>{e.title}</li>;
+				}) */}
+				
+				{/* refactor using fat arrow syntax */}
+				{todoList.map(
+					(e) => {
+						return <li key={e.id}>{e.title}</li>;
+					})
+				}
+			</ul>
+
+			{/* and by the way: that's how you do comments in JSX */}
 		</div>
 	)
 }
