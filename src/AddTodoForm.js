4e8cabd07cfc909e7fc263aa813298a4158d9906
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-04-28 21:30:50 -0700

    Updated AddTodoForm to preventDefault, retrieve value of title, console log it, and reset form.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 function AddTodoForm(){
 
+	function handleAddTodo(event){
+		event.preventDefault();
+		let todoTitle = event.target.title;
+		console.log(todoTitle);
+		event.target.title = '';
+	}
+
 	return (
 		<div>
 			<form>
 				<label htmlFor="todoTitle">Title</label>
-				<input id="todoTitle" />
+				<input id="todoTitle" name="title" />
 				<button type="button">Add</button>
 			</form>
 		</div>
