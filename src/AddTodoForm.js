a81d410cd758c44d788b2b8c344bcad9aea429fc
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-04-28 21:38:31 -0700

    Finished 'Add Todo Form Submit' section. Entering into text input field will log to console.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -4,14 +4,15 @@ function AddTodoForm(){
 
 	function handleAddTodo(event){
 		event.preventDefault();
-		let todoTitle = event.target.title;
+		let todoTitle = event.target.title.value;
 		console.log(todoTitle);
 		event.target.title = '';
+		/* logs input text value into console, but only works when I press "Enter" keyboard. Click the "Add" button does nothing. */
 	}
 
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleAddTodo}>
 				<label htmlFor="todoTitle">Title</label>
 				<input id="todoTitle" name="title" />
 				<button type="button">Add</button>
