4094f5b60d26d8bf85df3f3cc58b3957653085c5
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-05-13 20:33:08 -0700

    Finished handleTitleChange() function.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 function AddTodoForm(props){
+	
+	//Create new state variable named todoTitle with setter setTodoTitle
+	const [todoTitle, setTodoTitle] = useState('');
+
+	//Add onChange prop equal to handleTitleChange function reference (we will declare this function in the next step)
+	function handleTitleChange(event){
+		const newTodoTitle = event.target.value;
+		//console.log(newTodoTitle);
+		setTodoTitle(newTodoTitle);
+	}
 
 	function handleAddTodo(event){
 		event.preventDefault();
@@ -12,11 +23,16 @@ function AddTodoForm(props){
 		props.onAddTodo(todoTitle);
 	}
 
+	/*
+	Modify the <input> element to be a controlled input
+		Add value prop equal to todoTitle from component props
+	  Add onChange prop equal to handleTitleChange function reference (we will declare this function in the next step)
+	*/
 	return (
 		<div>
 			<form onSubmit={handleAddTodo}>
 				<label htmlFor="todoTitle">Title</label>
-				<input id="todoTitle" name="title" />
+				<input id="todoTitle" name="title" value={props.todoTitle} onChange={handleTitleChange}/>
 				<button type="button">Add</button>
 			</form>
 		</div>
