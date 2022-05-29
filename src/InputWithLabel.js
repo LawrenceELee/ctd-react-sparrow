cd8bc6e515d6651ecddee6944b4aa6ba696b4f1f
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-05-28 20:53:53 -0700

    Finished 'Add Auto-Focus to Input'.

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,11 +1,22 @@
+import { useRef, useEffect } from 'react';
+
 function InputWithLabel(props){
 
-	//Refectoring: use children
-	//Added autoFocus to input (noted that the 'F' is capitalized).
+	//Use the useRef React hook to create an imperative ref named inputRef
+	const inputRef = useRef();
+
+	//Define a useEffect React hook with an empty dependency list
+	//note: you need to use "current"
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
+	/* there might be a bug: it doesn't seem to autoFocus when I click the "Add" button. */
+
+
 	return (
 		<>
 			<label htmlFor={props.todoTitle} >{props.children}</label>
-			<input id={props.todoTitle} name="title" value={props.todoTitle} onChange={props.handleTitleChange} autoFocus />
+			<input id={props.todoTitle} name="title" value={props.todoTitle} onChange={props.handleTitleChange} ref={inputRef} />
 		</>
 	);
 
