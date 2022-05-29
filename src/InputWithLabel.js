78de44996e9c731eae26e68314f43eb50f5d3874
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-05-28 20:46:25 -0700

    Added autoFocus to input field.

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,11 +1,11 @@
-//Declare and export a new functional React component named
 function InputWithLabel(props){
 
 	//Refectoring: use children
+	//Added autoFocus to input (noted that the 'F' is capitalized).
 	return (
 		<>
-			<label htmlFor={props.todoTitle}>{props.children}</label>
-			<input id={props.todoTitle} name="title" value={props.todoTitle} onChange={props.handleTitleChange} />
+			<label htmlFor={props.todoTitle} >{props.children}</label>
+			<input id={props.todoTitle} name="title" value={props.todoTitle} onChange={props.handleTitleChange} autoFocus />
 		</>
 	);
 
