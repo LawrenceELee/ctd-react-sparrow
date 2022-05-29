02fea0cadc3192f9ced40ccd43d4c6752d5f9410
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-05-28 20:14:35 -0700

    Finished 'Replace label prop with children so that any child node(s) are used as the label text'

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,12 +1,10 @@
 //Declare and export a new functional React component named
 function InputWithLabel(props){
 
-	//Move label and input JSX from AddTodoForm.js to InputWithLabel.js
-	//Replace the text inside the label element with a new props variable named label
-	//Pass a label prop to the InputWithLabel component with value "Title"
+	//Refectoring
 	return (
 		<>
-			<label htmlFor={props.todoTitle}>{props.label}</label>
+			<label htmlFor={props.todoTitle}>{props.children}</label>
 			<input id={props.todoTitle} name="title" value={props.todoTitle} onChange={props.handleTitleChange}/>
 		</>
 	);
