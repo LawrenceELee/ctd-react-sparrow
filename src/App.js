13e5ecbd94c5b9910fba92bda6c09af4d657aeff
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-05-29 19:52:55 -0700

    Finished mimicing/mocking a fetch request and processing it using a Promise object.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,36 @@ import AddTodoForm from './AddTodoForm';
 
 function App() {
 	
-	const [newTodo, setNewTodo] = useState('');
-	
 	const [todoList, setTodoList] = useState( JSON.parse(localStorage.getItem("savedTodoList")) );
+
+	//Below the todoList state, define a useEffect React hook with an empty dependency list
+	useEffect(() => {
+		//Inside the side-effect handler function, define a new Promise and pass in a callback function with parameters resolve and reject
+		return new Promise((resolve, reject) => {
+
+			//To mimic a loading delay, inside the callback function declare a timeout (hint: setTimeout method) with the following arguments:
+			setTimeout(() => {
+
+				//Inside the timeout callback function, call the parameter resolve which is a callback function for when the Promise is successful and pass it an Object with property data as a nested empty Object
+				resolve({
+					//Inside the data object, add a property todoList and set it's value to the initial/default list state (copy from useState hook)
+					//Update the default state for todoList to be an empty Array
+					data: { 
+						todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [], 
+					}
+				});
+
+			}, 2000);
+
+		//Chain a then method to your Promise constructor and pass it a function with parameter result
+		}).then((result) => {
+			//Inside the function, use your state setter to update the list and pass the todoList from your result object
+			setTodoList(result.data.todoList);
+		});
+	});
+
+	//Need to review: So what's missing? We are retrieving our list from localStorage but we aren't updating our todoList state with the data so it remains empty. Let's fix that:
+	//I don't see this behavior
 
 	//Define a useEffect React hook with todoList as a dependency
 	useEffect(() => {
